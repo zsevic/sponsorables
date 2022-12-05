b99284b79acf79353441f87678261887bd5f4baa
zsevic <zsevic95@gmail.com>
2022-12-05 12:29:14 +0100

    Fix errors

--- src/pages/api/sponsorables.js
+++ src/pages/api/sponsorables.js
@@ -1,10 +1,10 @@
-import { getSponsorables } from 'services/github-api';
+import { getSponsorables } from "services/github-api";
 
 export default async function handler(req, res) {
   const { location } = req.query;
   if (!location) {
-    return res.status(400).json({ message: 'Location parameter is required' });
+    return res.status(400).json({ message: "Location parameter is required" });
   }
   const sponsorables = await getSponsorables(location);
-  res.status(200).json({ data: sponsorables });
+  return res.status(200).json({ data: sponsorables });
 }
