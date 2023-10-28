e91b564012626a54c62124c8c223a29812033a9e
zsevic <zsevic95@gmail.com>
2023-10-28 22:35:38 +0200

    Update analytics code

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,4 +1,4 @@
 export const API_BASE_URL =
   process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8080";
 
-export const ANALYTICS_TRACKING_ID = "G-9MYNBB7MHS";
+export const ANALYTICS_TRACKING_ID = "G-3RD6EEERSJ";
