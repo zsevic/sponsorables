6ff4fc90f6ef2088e21720e89d22dab7c5c6b64b
zsevic <zsevic95@gmail.com>
2023-10-28 22:19:21 +0200

    Set up analytics

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,12 @@
 import PropTypes from "prop-types";
 import React from "react";
+import ReactGA from "react-ga4";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { ANALYTICS_TRACKING_ID } from "constants/config";
+
+if (process.env.NODE_ENV !== "development") {
+  ReactGA.initialize(ANALYTICS_TRACKING_ID);
+}
 
 function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
